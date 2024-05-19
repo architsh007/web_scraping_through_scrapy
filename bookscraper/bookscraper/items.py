921c2cb11bc7fbf980f212fd4c53ccee415e1090
Archit Sharma <architsh007@gmail.com>
2024-05-19 16:43:31 +0530

    second addition

--- bookscraper/bookscraper/items.py
+++ bookscraper/bookscraper/items.py
@@ -6,7 +6,18 @@
 import scrapy
 
 
-class BookscraperItem(scrapy.Item):
+class BookItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    book_url = scrapy.Field()
+    book_name = scrapy.Field()
+    book_category = scrapy.Field()
+    rating = scrapy.Field()
+    upc = scrapy.Field()
+    product_type = scrapy.Field()
+    price_excl_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    number_of_reviews = scrapy.Field()
+    description = scrapy.Field()
+
--- bookscraper/bookscraper/spiders/book_detailed_spider.py
+++ bookscraper/bookscraper/spiders/book_detailed_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from bookscraper.items import BookItem
 
 class BookDetailedSpiderSpider(scrapy.Spider):
     name = "book_detailed_spider"
@@ -31,17 +31,18 @@ class BookDetailedSpiderSpider(scrapy.Spider):
             yield response.follow(next_page_url, callback=self.parse)
 
     def parse_book(self, response):
-        yield{
-            'book url' : response.url,
-            'book name' :  response.css('li.active ::text').get(),
-            'book category' : response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
-            'rating' : response.css('a::text')[-1].get(),
-            'upc' : response.css('table tr td::text')[0].get(),
-            'product type' : response.css('table tr td::text')[1].get(),
-            'Price (excl. tax)' : response.css('table tr td::text')[2].get(),
-            'Price (incl. tax)' : response.css('table tr td::text')[3].get(),
-            'Tax' : response.css('table tr td::text')[4].get(),
-            'Availability' : response.css('table tr td::text')[5].get(),
-            'Number of reviews' : response.css('table tr td::text')[6].get()
-        }
-        
+        book_item = BookItem()
+        book_item['book_url'] = response.url
+        book_item['book_name'] =  response.css('li.active ::text').get()
+        book_item['book_category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get()
+        book_item['rating'] = response.css('a::text')[-1].get()
+        book_item['upc'] = response.css('table tr td::text')[0].get()
+        book_item['product_type'] = response.css('table tr td::text')[1].get()
+        book_item['price_excl_tax'] = response.css('table tr td::text')[2].get()
+        book_item['price_incl_tax'] = response.css('table tr td::text')[3].get()
+        book_item['tax'] = response.css('table tr td::text')[4].get()
+        book_item['availability'] = response.css('table tr td::text')[5].get()
+        book_item['number_of_reviews'] = response.css('table tr td::text')[6].get()
+        book_item['description'] = response.xpath("//div[@class='sub-header']/following-sibling::p/text()").get()
+    
+        yield book_item
